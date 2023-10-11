e897998b62b52c647c04d5f75b47c1cfe64ff437
David Pagliano <david.s.pagliano@gmail.com>
2023-10-11 08:13:37 -0300

    dPagliano: Agregue unos comentarios en el index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+//Librerias
 import express from "express";
 import bodyParser from "body-parser";
 
+//Routes
 import clienteRoutes from "./routes/routes_cliente.js";
-import productosRoutes from "./routes/routes_producto.js"
+import productosRoutes from "./routes/routes_producto.js";
+import ventasRoutes from "./routes/routes_ventas.js";
 
+//Middleware
 const app = express();
 const PORT = 3000;
 app.use(bodyParser.json());
 
 app.use("/cliente", clienteRoutes);
 app.use("/producto",productosRoutes);
+app.use("/venteas", ventasRoutes);
 app.get("/", (req, res) => res.send("Welcome to the API RFEST!"));
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
